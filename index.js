500a5e1cef78e880fd792101768f24d0d3958309
Ilya Petrenko <gr0x562@gmail.com>
2020-04-29 22:11:32 +0300

    node http in index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const chalk = require('chalk');
-const text = require('./data');
+const http = require('http');
 
-console.log(chalk.blue(text));
-console.log(__dirname);
+const server = http.createServer((req, res) => {
+  res.end('<h1>Hello NodeJS</h1>');
+});
+
+server.listen(3333, () => {
+  console.log('Server has been started...');
+})
