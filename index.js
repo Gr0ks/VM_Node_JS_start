89d504d3aa748e8a730696c0c30c6cb916e26d2d
Ilya Petrenko <gr0x562@gmail.com>
2020-04-29 22:30:06 +0300

    first page server

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.end('<h1>Hello NodeJS</h1>');
+  
+
+  if(req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+      if(err) {
+        throw err;
+      }
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    })
+  } else if(req.url === '/contact') {
+    fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+      if(err) {
+        throw err;
+      }
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    })
+  };
 });
 
 server.listen(3333, () => {
