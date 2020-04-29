4a57fb7de57b6f00f62df450f6a630193739c71b
Ilya Petrenko <gr0x562@gmail.com>
2020-04-29 22:47:31 +0300

    rework page server

--- index.js
+++ index.js
@@ -3,29 +3,58 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  
+  // if(req.url === '/') {
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+  //     if(err) {
+  //       throw err;
+  //     }
+  //     res.writeHead(200, {
+  //       'Content-Type': 'text/html'
+  //     });
+  //     res.end(data);
+  //   })
+  // } else if(req.url === '/contact') {
+  //   fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+  //     if(err) {
+  //       throw err;
+  //     }
+  //     res.writeHead(200, {
+  //       'Content-Type': 'text/html'
+  //     });
+  //     res.end(data);
+  //   })
+  // };
 
-  if(req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-      if(err) {
-        throw err;
-      }
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  const ext = path.extname(filePath);
+  if(!ext) {
+    filePath += '.html';
+  }
+
+  console.log(filePath);
+
+  fs.readFile(filePath, (err, content) => {
+    if(err) {
+      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+        if(err) {
+          res.writeHead(500);
+          res.end('Server ERROR');
+        } else {
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
+          res.end(data);
+        }
+
+      })
+    } else {
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      res.end(data);
-    })
-  } else if(req.url === '/contact') {
-    fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
-      if(err) {
-        throw err;
-      }
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      res.end(data);
-    })
-  };
+      res.end(content);
+    }
+  });
 });
 
 server.listen(3333, () => {
